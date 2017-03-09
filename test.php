407d36f6fdf0c2d4e2ec6b6490ce51796f476c08
atonizer <atonizer@users.noreply.github.com>
2017-03-09 14:43:57 +0300

    untinled php project

---
+++ test.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stargazer
+ * Date: 09.02.2017
+ * Time: 13:33
+ */
+
+//phpinfo();
+
+function pre($array)
+{
+    echo '<pre>';
+    print_r($array);
+    echo "</pre>";
+}
+
+
---
+++ testphpunit.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Stargazer
+ * Date: 10.02.2017
+ * Time: 13:55
+ */
+
+class SomeTest extends PHPUnit_Framework_TestCase
+{
+    public function testSomething()
+    {
+       $this->assertTrue(false);
+    }
+}
