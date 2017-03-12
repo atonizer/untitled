8badd988c99c4addc1d68821c1bf18870c0c8bed
atonizer <atonizer@users.noreply.github.com>
2017-03-12 11:16:47 +0300

    untitled php project

--- tests/AssertionsTest.php
+++ tests/AssertionsTest.php
@@ -13,16 +13,42 @@ class AssertionsTest extends PHPUnit_Framework_TestCase
         //$this->assertEquals(1 + 5, 6);
         //$this->assertInstanceOf('DateTime', new DateTime());
         //$this->assertNotInstanceOf('DateTime', new DateTime());
-        $arr = array();
+ /*       $arr = array();
         $this->assertCount(0, $arr);
         $arr[] = 2;
         $this->assertCount(1, $arr);
         $this->assertContains(2, $arr);
         $arr[] = 12;
         $arr[] = 65;
-        $this->assertCount(3, $arr);
+        $this->assertCount(3, $arr);*/
 
         // Start 05-Exceptions.mov
+
+    }
+
+    /**
+     * @expectedException LogicException
+     * @expectedExceptionMessage Foos\
+     * @expectedExceptionCode 1234
+     *
+     */
+    public function testException()
+    {
+        throw new LogicException('Foo', 1234);
+
+/*        try
+        {
+            throw new LogicException('Foo', 1234);
+            $this->fail('We should not be here');
+
+        }
+        catch(LogicException $e)
+        {
+            $this->assertEquals($e->getMessage(), 'Foo');
+            $this->assertEquals($e->getCode(), 1234);
+        }*/
+
+     //   06-Errors-Warnings-and-Notices
 
     }
 }
