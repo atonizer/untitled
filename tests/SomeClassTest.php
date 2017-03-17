e84586503f2a1134f60aa3b754c6e012c43d7241
atonizer <atonizer@users.noreply.github.com>
2017-03-17 12:37:55 +0300

    untitled php project

---
+++ tests/SomeClassTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class SomeClassTest extends PHPUnit_Framework_TestCase
+{
+    public function testSomethingThatDontExistsNow()
+    {
+        $this->markTestIncomplete('Waiting for implementation');
+    }
+
+    public function testSomethingOptional()
+    {
+        if(!extension_loaded('libevent'))
+        {
+            $this->markTestSkipped('Install libevent');
+        }
+    }
+}
